9430cfb19ea0b6ec92a0897a1e207ec8405a33ab
sujith <mr.c.sujithreddy@gmail.com>
2022-09-15 18:01:51 +0530

    done

---
+++ Library/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class Book
+    {
+
+        public Guid Id { get; set; }
+        [Required]
+     
+       
+        public string Title { get; set; }
+        public string Author { get; set; }
+        
+        public string Description { get; set; }
+
+    }
+}
---
+++ Library/Services/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using Library.Models;
+
+namespace Library.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAll();
+
+        Book Add(IBookService newBook);
+
+        Book GetById(Guid id);
+
+        void Remove(Guid id);
+
+        Book Add(Book value);
+    }
+}
